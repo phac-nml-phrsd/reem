0e8caab6976ebc278618f165d8aefbd0299b1ad1
David Champredon <david.champredon@gmail.com>
2024-05-22 08:15:51 -0400

    add to tests simulate_epi

--- tests/testthat/test-simulate_epi.R
+++ tests/testthat/test-simulate_epi.R
@@ -3,6 +3,9 @@ test_that("simulate_epi works", {
   date.start = lubridate::ymd('2022-01-01')
   asof       = lubridate::ymd('2022-03-01') 
   
+  ## DO NOT CHANGE PARAMETERS!
+  ## (if changed, then adjust tests on numerical values accordingly)
+  
   prms = list(
     horizon = 120,  # horizon of the simulation
     last.obs = 299,  # last observation time (must be < horizon)
@@ -49,4 +52,18 @@ test_that("simulate_epi works", {
   check.names.sim = all(names(sim) %in% c("t","m","I","S","A","Y","Wd","Wp","Wr","date"))
   expect_true(check.names.sim)
   expect_equal(nrow(sim), prms$horizon)
+  
+  # ---  Now check that the numerical output values make sense
+
+  # Deterministic and set seed for replication
+  set.seed(1234)
+  simepi2  = obj$simulate_epi(deterministic = TRUE)
+  sim2 = simepi2$sim  
+  plot(sim2$S)
+  Imax = round(max(sim2$I))   
+  Ymax = round(max(sim2$Y))   
+  Smin = round(min(sim2$S)) 
+  expect_equal(Imax, 139)
+  expect_equal(Ymax, 109)
+  expect_equal(Smin, 4912)  
 })
